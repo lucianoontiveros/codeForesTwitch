c4947b4694f5fa2c246b55e97abeec17c1cdff58
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2023-02-15 19:34:25 -0300

    tuvimos que cambiar todoa la logica del timer producto de estabamos teniendo serios problemas de rendimiento, por lo cual el codigo del mensaje emergente se quedaba en pantalla sin volver a componente banner. Solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ import Banner from './Components/Banner.jsx';
 
 
 function App() {
-  const [code, setCode] = useState(false);
-  const [timer, setTimer] = useState(null);
-  const [codigoSala, setCodigoSala] = useState('')
-  const [aviso, setAviso] = useState("Un canal de estudio y trabajo  #tercermundista")
+  const [codigoSala, setCodigoSala] = useState('');
+  const [showCode, setShowCode] = useState(false);
+  const [aviso, setAviso] = useState("Un canal de estudio y trabajo  #tercermundista");
 
   useEffect(() => {
     const client = new tmi.Client({
@@ -27,7 +26,8 @@ function App() {
 
     client.connect();
 
-    let timerId = null;
+    let timerId;
+    var codigoActual = '';
 
     client.on("message", (channel, userstate, message, self) => {
       if (self) return;
@@ -35,35 +35,37 @@ function App() {
       const isMod = userstate.badges?.moderator
       const mod = userstate?.mod;
       if (self || !message.startsWith('!')) return;
+
       var args = message.slice(1).split(' ');
       const command = args.shift().toLowerCase();
+
       if (command === "codigo") {
         if (username === 'cuartodechenz' || mod) {
           console.log(args)
           setCodigoSala(args)
-          setCode(true);
-          if (timerId) {
-            clearTimeout(timerId);
-          }
+          codigoActual = args;
+          setShowCode(true);
+          clearTimeout(timerId);
           timerId = setTimeout(() => {
-            setCode(false);
+            setShowCode(false);
             timerId = null;
-          }, 780000);
+          }, 480000);
         }
       }
 
       switch (command) {
         case 'aviso':
-          setAviso(message)
+          setAviso(message);
           break;
         case 'code':
           if (username === 'cuartodechenz' || mod) {
             args = '';
-            client.say(channel, `Este es el codigo de la salita actual para no quedarte afuera https://www.forestapp.cc/join-room?token=${codigoSala}`)
+            const code = `https://www.forestapp.cc/join-room?token=${codigoActual}`;
+            client.say(channel, `Este es el codigo de la salita actual para no quedarte afuera ${code}`);
           }
           break;
         default:
-          console.log('La peticion no puede realizarse')
+          console.log('La petición no puede realizarse');
       }
     });
 
@@ -73,17 +75,13 @@ function App() {
         clearTimeout(timerId);
       }
     };
-  }, [codigoSala]);
+  }, []);
 
   return (
     <div>
-      {code ? (
-        <Code codigoSala={codigoSala} />
-      ) : (
-        <Banner aviso={aviso} />
-      )}
+      {showCode ? <Code codigoSala={codigoSala} /> : <Banner aviso={aviso} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
